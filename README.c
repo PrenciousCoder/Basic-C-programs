715a292096f122a697ccffd7ebb53dff8256d17f
PrenciousCoder <87071081+PrenciousCoder@users.noreply.github.com>
2021-07-22 12:44:27 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,35 @@
+# Basic-C-programs
+Access all Beginner level C programs. including each and every small topics
+//
+//  main.c
+//  Printf
+//
+//  Created by Devansh on 24/04/21.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("my favourite number is %d\n", 500);
+    printf("%d\n",500);
+    printf("my favourite %s is %d\n", "number", 500);
+    printf("i hate the %s %F\n", "number", 0.007);
+    int favnum = 90.9;
+    printf("my favourite %s is %d\n", "number", favnum);
+    char mychar[2] = "y";
+    printf("my favourite %s is %d\n", mychar, favnum);
+    int a = 853;
+    printf("the best %s is %d\n", "number", a);
+    const int numm = 89;
+    printf("a good %s is %d\n", "number", numm);
+    
+    
+
+    
+
+
+
+
+    return 0;
+}
