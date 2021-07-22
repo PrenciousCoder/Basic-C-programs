0043329fa69462bb9d474af9ed372c8a61196765
PrenciousCoder <87071081+PrenciousCoder@users.noreply.github.com>
2021-07-22 12:56:58 +0530

    Tictactoe.c

--- README.c
+++ README.c
@@ -1,35 +1,113 @@
-# Basic-C-programs
-Access all Beginner level C programs. including each and every small topics
 //
 //  main.c
-//  Printf
+//  Tictactoe
 //
-//  Created by Devansh on 24/04/21.
+//  Created by Devansh on 22/07/21.
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <ncurses_dll.h>
+#include <readpassphrase.h>
+#include <stdlib.h>
+#include <curses.h>
+
+
+char square[10]={'o','1','2','3','4','5','6','7','8','9'};
+int checkwin();
+void drawBoard();
 
 int main()
 {
-    printf("my favourite number is %d\n", 500);
-    printf("%d\n",500);
-    printf("my favourite %s is %d\n", "number", 500);
-    printf("i hate the %s %F\n", "number", 0.007);
-    int favnum = 90.9;
-    printf("my favourite %s is %d\n", "number", favnum);
-    char mychar[2] = "y";
-    printf("my favourite %s is %d\n", mychar, favnum);
-    int a = 853;
-    printf("the best %s is %d\n", "number", a);
-    const int numm = 89;
-    printf("a good %s is %d\n", "number", numm);
+    int player=1,i,choice;
+    char mark;//X or O
+    do{
+        drawBoard();
+        player=(player%2)?1:2;
+        printf("player %d enter the choice :", player);
+        scanf("%d", &choice);
+        mark=(player==1)? 'X':'O';
+        if( choice ==1 && square[1]=='1')
+            square[1]=mark;
+        else if( choice == 2 && square[2] = '2')
+            square[2]=mark;
+        else if( choice == 3 && square[3] = '3')
+            square[3]=mark;
+        else if( choice == 4 && square[4] = '4')
+            square[4]=mark;
+        else if( choice == 5 && square[5] = '5')
+            square[5]=mark;
+        else if( choice == 6 && square[5] = '6')
+            square[6]=mark;
+        else if( choice == 7 && square[7] = '7')
+            square[7]=mark;
+        else if( choice == 8 && square[8] = '8')
+            square[8]=mark;
+        else if( choice == 9 && square[9] = '9')
+            square[9]=mark;
+        
+        else{
+            printf("invalid option!");
+            player--;
+            getch();
+        }
+        i=checkwin();
+        player++;
+    }while(i==-1);
+    
+    drawBoard();
+    
+    if(i==1){
+        printf("player %d==won", --player);
+               
+    }
+               else {
+                   printf("==>Game draw");
+        }
+    getch();
     
     
-
     
-
-
-
-
     return 0;
 }
+int checkwin(){
+    if(square[1]==square[2]&&square[2]==square[3])
+        return 1;
+    else if(square[4]==square[5] && square[5]==square[6])
+        return 1;
+    else if(square[7]==square[8] && square[8]==square[9])
+        return 1;
+    else if(square[1]==square[4] && square[4]==square[7])
+        return 1;
+    else if(square[2]==square[5] && square[5]==square[8])
+        return 1;
+    else if(square[3]==square[6] && square[6]==square[8])
+        return 1;
+    else if(square[1]==square[5] && square[5]==square[9])
+        return 1;
+    else if(square[3]==square[5] && square[5]==square[7])
+        return 1;
+    else if(square[1]!='1' && square [2] !='2' && square [3] !='3' && square [4] !='4' && square [5] !='5' && square [6] !='6' && square [7] !='7' && square [8] !='8' &&
+            square [9] !='9')
+        return 0;
+    else
+        return -1;
+}
+
+void drawBoard(){
+    system("cls");
+    printf("\n\n\t Tic Tac Toe \n\n");
+    printf("player1 (X) - player2 (O) \n\n\n");
+    printf("   |   |   \n");
+    printf("  %c  |  %c  |  %c  \n", square[1],square[2],square[3]);
+    printf("____|____|____\n");
+    printf("____|____|____\n");
+    printf("  %c  |  %c  |  %c  \n", square[4],square[5],square[6]);
+    printf("____|____|____\n");
+    printf("____|____|____\n");
+    printf("  %c  |  %c  |  %c  \n", square[7],square[8],square[9]);
+    printf("   |   |   \n");
+    
+    
+}
+
